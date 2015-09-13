c9d4e3ab55f4b5ff77fd72bd99c5ea9c3aa8dda9
yydxlv <yydxlv@qq.com>
2015-09-13 21:44:58 +0800

    Machine learning in action

--- Sklearn应用/Simple Grasp with Sklearn.py
+++ Sklearn应用/Simple Grasp with Sklearn.py
@@ -11,13 +11,15 @@ def prepare_data_train(fname):
     """ read and prepare training data """
     # Read data
     data = pd.read_csv(fname)
+    print(data)
     # events file
-    events_fname = fname.replace('_data','_events')
+    events_fname = fname.replace('_data', '_events')
+    print(events_fname)
     # read event file
     labels= pd.read_csv(events_fname)
-    clean=data.drop(['id' ], axis=1)  # remove id
-    labels=labels.drop(['id' ], axis=1)  # remove id
-    return  clean,labels
+    clean=data.drop(['id'], axis=1)  # remove id
+    labels=labels.drop(['id'], axis=1)  # remove id
+    return clean,labels
 
 def prepare_data_test(fname):
     """ read and prepare test data """
@@ -53,7 +55,8 @@ for subject in subjects:
     y_raw= []
     raw = []
     #  READ DATA
-    fnames = glob('../input/train/subj%d_series*_data.csv' % subject)
+    # fnames = glob('../input/train/subj%d_series*_data.csv' % subject)
+    fnames = glob('data.csv' % subject)
     for fname in fnames:
       data,labels=prepare_data_train(fname)
       raw.append(data)
@@ -61,15 +64,15 @@ for subject in subjects:
 
     X = pd.concat(raw)
     y = pd.concat(y_raw)
-    #transform in numpy array
-    #transform train data in numpy array
+    # transform in numpy array
+    # transform train data in numpy array
     X_train =np.asarray(X.astype(float))
     y = np.asarray(y.astype(float))
 
 
-    ################ Read test data #####################################
-    #
-    fnames =  glob('../input/test/subj%d_series*_data.csv' % (subject))
+ # Read test data
+ #    fnames = glob('../input/test/subj%d_series*_data.csv' % subject)
+    fnames = glob('data.csv')
     test = []
     idx=[]
     for fname in fnames:
@@ -79,12 +82,12 @@ for subject in subjects:
     X_test= pd.concat(test)
     ids=np.concatenate(idx)
     ids_tot.append(ids)
-    X_test=X_test.drop(['id' ], axis=1)#remove id
-    #transform test data in numpy array
+    X_test=X_test.drop(['id' ], axis=1)  # remove id
+    # transform test data in numpy array
     X_test =np.asarray(X_test.astype(float))
 
 
-    ################ Train classifiers ########################################
+ # Train classifiers
     lr = LogisticRegression()
     pred = np.empty((X_test.shape[0],6))
     X_train=data_preprocess_train(X_train)
@@ -94,14 +97,16 @@ for subject in subjects:
         print('Train subject %d, class %s' % (subject, cols[i]))
         lr.fit(X_train[::subsample,:],y_train[::subsample])
         pred[:,i] = lr.predict_proba(X_test)[:,1]
-
     pred_tot.append(pred)
 
 # submission file
 submission_file = 'grasp-sub-simple.csv'
+
 # create pandas object for sbmission
 submission = pd.DataFrame(index=np.concatenate(ids_tot),columns=cols,data=np.concatenate(pred_tot))
 
 # write file
 submission.to_csv(submission_file,index_label='id',float_format='%.3f')
-# if __name__=="__main__":
+
+if __name__=="__main__":
+    prepare_data_train("data.csv")
