0bc2b772e88fbf997272421ae7374f69d2b2237a
yydxlv <yydxlv@qq.com>
2015-10-17 20:06:59 +0800

    Classifiers performance comparsion

--- Otto Group Product Classification/Why calibration works.py
+++ Otto Group Product Classification/Why calibration works.py
@@ -16,47 +16,51 @@ from sklearn.calibration import CalibratedClassifierCV
 # See http://scikit-learn.org/stable/modules/calibration.html for more details
 # This script is an example of how to implement calibration, and check if it boosts performance.
 
-# Import Data
-# X = pd.read_csv('../input/train.csv')
-X = pd.read_csv('train.csv')
-X = X.drop('id', axis=1)
 
-# Extract target
-# Encode it to make it manageable by ML algo
-y = X.target.values
-y = LabelEncoder().fit_transform(y)
+if __name__=="__main__":
+    # Import Data
+    # X = pd.read_csv('../input/train.csv')
+    X = pd.read_csv('train.csv')
+    X = X.drop('id', axis=1)
 
-# Remove target from train, else it's too easy ...
-X = X.drop('target', axis=1)
+    # Extract target
+    # Encode it to make it manageable by ML algo
+    y = X.target.values
+    print(y)
+    y = LabelEncoder().fit_transform(y)
+    print(y)
 
-# Split Train / Test
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.20, random_state=36)
+    # Remove target from train, else it's too easy ...
+    X = X.drop('target', axis=1)
 
-# First, we will train and apply a Random Forest WITHOUT calibration
-# we use a BaggingClassifier to make 5 predictions, and average
-# because that's what CalibratedClassifierCV do behind the scene,
-# and we want to compare things fairly, i.e. be sure that averaging several models
-# is not what explains a performance difference between no calibration, and calibration.
+    # Split Train / Test
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.20, random_state=36)
 
-clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
-clfbag = BaggingClassifier(clf, n_estimators=5)
-clfbag.fit(Xtrain, ytrain)
-ypreds = clfbag.predict_proba(Xtest)
-print("loss WITHOUT calibration : ", log_loss(ytest, ypreds, eps=1e-15, normalize=True))
+    # First, we will train and apply a Random Forest WITHOUT calibration
+    # we use a BaggingClassifier to make 5 predictions, and average
+    # because that's what CalibratedClassifierCV do behind the scene,
+    # and we want to compare things fairly, i.e. be sure that averaging several models
+    # is not what explains a performance difference between no calibration, and calibration.
 
-# Now, we train and apply a Random Forest WITH calibration
-# In our case, 'isotonic' worked better than default 'sigmoid'
-# This is not always the case. Depending of the case, you have to test the two possibilities
+    clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
+    clfbag = BaggingClassifier(clf, n_estimators=5)
+    clfbag.fit(Xtrain, ytrain)
+    ypreds = clfbag.predict_proba(Xtest)
+    print("loss WITHOUT calibration : ", log_loss(ytest, ypreds, eps=1e-15, normalize=True))
 
-clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
-calibrated_clf = CalibratedClassifierCV(clf, method='isotonic', cv=5)
-calibrated_clf.fit(Xtrain, ytrain)
-ypreds = calibrated_clf.predict_proba(Xtest)
-print("loss WITH calibration : ", log_loss(ytest, ypreds, eps=1e-15, normalize=True))
+    # Now, we train and apply a Random Forest WITH calibration
+    # In our case, 'isotonic' worked better than default 'sigmoid'
+    # This is not always the case. Depending of the case, you have to test the two possibilities
 
-print(" ")
-print("Conclusion : in our case, calibration improved performance a lot ! (reduced loss)")
+    clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
+    calibrated_clf = CalibratedClassifierCV(clf, method='isotonic', cv=5)
+    calibrated_clf.fit(Xtrain, ytrain)
+    ypreds = calibrated_clf.predict_proba(Xtest)
+    print("loss WITH calibration : ", log_loss(ytest, ypreds, eps=1e-15, normalize=True))
 
-# We can see that we highly improved performance with calibration (loss is reduced) !
-# Using calibration helped our team a lot to climb the leaderboard.
-# In the future competitions, that's for sure, I will not forget to test this trick !
+    print(" ")
+    print("Conclusion : in our case, calibration improved performance a lot ! (reduced loss)")
+
+    # We can see that we highly improved performance with calibration (loss is reduced) !
+    # Using calibration helped our team a lot to climb the leaderboard.
+    # In the future competitions, that's for sure, I will not forget to test this trick !
